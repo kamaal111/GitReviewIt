beb4606c43a9cae2d175f1a192631c74f2972d92
Kamaal Farah <kamaal.f1@gmail.com>
2025-12-20 19:48:09 +0100

    Initial commit

---
+++ GitReviewIt/GitReviewItApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitReviewItApp.swift
+//  GitReviewIt
+//
+//  Created by Kamaal M Farah on 12/20/25.
+//
+
+import SwiftUI
+
+@main
+struct GitReviewItApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
